d3e4012093945ed32d0bbf47d22738f4daa68fb4
Hugo Oliveira <hugo684@gmail.com>
2021-12-29 11:19:27 -0300

    Implemented parallel execution for the report building process

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -2,6 +2,7 @@ defmodule GenReport do
   # nome, quantidade de horas, dia, mês e ano.
 
   alias GenReport.Parser
+  alias GenReport.MapUtils
 
   def build(filename) do
     {_, report} =
@@ -21,6 +22,41 @@ defmodule GenReport do
 
   def build() do
     {:error, "Insira o nome de um arquivo"}
+  end
+
+  def build_from_many(filename_list) when not is_list(filename_list) do
+    {:error, "Please provide a list containing the filenames to be parsed."}
+  end
+
+  def build_from_many(filename_list) do
+    filename_list
+    |> Task.async_stream(&build/1)
+    |> Enum.reduce(generate_empty_report(), fn {:ok, result}, report ->
+      sum_reports(result, report)
+    end)
+  end
+
+  defp sum_reports(
+         %{
+           all_hours: all_hours1,
+           hours_per_month: hours_per_month1,
+           hours_per_year: hours_per_year1
+         },
+         %{
+           all_hours: all_hours2,
+           hours_per_month: hours_per_month2,
+           hours_per_year: hours_per_year2
+         }
+       ) do
+    all_hours = merge_maps(all_hours1, all_hours2)
+    hours_per_month = merge_maps(hours_per_month1, hours_per_month2)
+    hours_per_year = merge_maps(hours_per_year1, hours_per_year2)
+
+    %{all_hours: all_hours, hours_per_month: hours_per_month, hours_per_year: hours_per_year}
+  end
+
+  defp merge_maps(map1, map2) do
+    MapUtils.deep_merge(map1, map2)
   end
 
   defp count_all_hours(
@@ -51,5 +87,236 @@ defmodule GenReport do
     hours_per_year = put_in(hours_per_year, [name, year], hours_per_year[name][year] + hours)
 
     %GenReport.Report{report | hours_per_year: hours_per_year}
+  end
+
+  defp generate_empty_report() do
+    %{
+      all_hours: %{
+        "cleiton" => 0,
+        "daniele" => 0,
+        "danilo" => 0,
+        "diego" => 0,
+        "giuliano" => 0,
+        "jakeliny" => 0,
+        "joseph" => 0,
+        "mayk" => 0,
+        "rafael" => 0,
+        "vinicius" => 0
+      },
+      hours_per_month: %{
+        "cleiton" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "daniele" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "danilo" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "diego" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "giuliano" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "jakeliny" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "joseph" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "mayk" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "rafael" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        },
+        "vinicius" => %{
+          "janeiro" => 0,
+          "fevereiro" => 0,
+          "março" => 0,
+          "abril" => 0,
+          "maio" => 0,
+          "junho" => 0,
+          "julho" => 0,
+          "agosto" => 0,
+          "setembro" => 0,
+          "outubro" => 0,
+          "novembro" => 0,
+          "dezembro" => 0
+        }
+      },
+      hours_per_year: %{
+        "cleiton" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "daniele" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "danilo" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "diego" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "giuliano" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "jakeliny" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "joseph" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "mayk" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "rafael" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        },
+        "vinicius" => %{
+          2016 => 0,
+          2017 => 0,
+          2018 => 0,
+          2019 => 0,
+          2020 => 0
+        }
+      }
+    }
   end
 end
